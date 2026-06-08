e726327c0b5ac3133966f9f346b96fcdf3c075b7
EnzoToguti <enzo.toguti@escola.pr.gov.br>
2026-06-08 12:34:16 -0300

    correção

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-//-----------------AULA DE FUCTION------------------------------------------
-//function calcularPedido(preco, quantidade) {
-//  let total = preco * quantidade;
-//  return total;
-//};
-
 //let resultado = calcularPedido(12, 3);
 //console.log('Valor do pedido: ' + resultado);
 //---------------------------------------------------------------------------
@@ -33,7 +27,7 @@ for (let i = 0; i < numeros.length; i++) {
 console.log('Soma total: ' + soma); */
 
 //3
-let numeros2 = [15, 42, 7, 98, 23, 61, 4, 12, 67, 30];
+/*let numeros2 = [15, 42, 7, 98, 23, 61, 4, 12, 67, 30];
 
 let maior = numeros2[0];
 
@@ -48,20 +42,19 @@ console.log('Maior número: ' + maior);
 //-----------------------------------------------------------------
 
 //------------------------TUPLAS-----------------------------------
-/*//1
-let aluno = ['Carlos', 17, '3°A'];
+
+const aluno = ['Carlos', 17, '3°A'];
 
 console.log(aluno);
 
-//2
-let produto = ['Notebook', 3500, 12];
+const produto = ['Notebook', 3500, 12];
 
 console.log('Produto: ' + produto[0]);
 console.log('Preço: R$ ' + produto[1]);
 console.log('Estoque: ' + produto[2] + ' unidades');
 
-//3
-let filmes = [
+/*/
+const filmes = [
   ['Vingadores', 2019, 'Ação'],
   ['Toy Story', 1995, 'Animação'],
   ['Avatar', 2009, 'Ficção'],
@@ -72,6 +65,6 @@ for (let i = 0; i < filmes.length; i++) {
   console.log('Ano: ' + filmes[i][1]);
   console.log('Gênero: ' + filmes[i][2]);
   console.log('----------------');
-} */
+}
 
 //-----------------------------------------------------------------
